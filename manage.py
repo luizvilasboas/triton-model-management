3c94a3d69939e1a54dac62168cd4ce5a89010b4f
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-05-27 11:30:49 -0300

    Update manage.py to load JSON from .pbtxt

--- manage.py
+++ manage.py
@@ -2,39 +2,51 @@ import argparse
 import requests
 import json
 
-BASE_URL = "http://localhost:8000/v2/repository"
+BASE_URL = "http://localhost:8000/v2"
 
+def load_model(model, config):
+    if config:
+        from jsonify import pbtxt_to_dict
 
-def load_models(models):
-    for model in models:
-        url = f"{BASE_URL}/models/{model}/load"
+        config = pbtxt_to_dict(config)
+        config = json.dumps(config)
 
-        print(f"Loading model: {model}")
+        payload = {
+            "parameters": {
+                "config": config
+            }
+        }
 
+    url = f"{BASE_URL}/repository/models/{model}/load"
+
+    print(f"Loading model: {model}")
+
+    if config:
+        response = requests.post(url, json=payload)
+    else:
         response = requests.post(url)
 
-        if response.status_code == 200:
-            print(f"Successfully loaded model: {model}")
-        else:
-            print(f"Failed to load model: {model}. Status code: {response.status_code}")
+    if response.status_code == 200:
+        print(f"Successfully loaded model: {model}")
+    else:
+        print(f"Failed to load model: {model}. Status code: {response.status_code}")
 
 
-def unload_models(models):
-    for model in models:
-        url = f"{BASE_URL}/models/{model}/unload"
+def unload_model(model):
+    url = f"{BASE_URL}/repository/models/{model}/unload"
 
-        print(f"Unloading model: {model}")
+    print(f"Unloading model: {model}")
 
-        response = requests.post(url)
+    response = requests.post(url)
 
-        if response.status_code == 200:
-            print(f"Successfully unloaded model: {model}")
-        else:
-            print(f"Failed to unload model: {model}. Status code: {response.status_code}")
+    if response.status_code == 200:
+        print(f"Successfully unloaded model: {model}")
+    else:
+        print(f"Failed to unload model: {model}. Status code: {response.status_code}")
 
 
 def list_models():
-    url = f"{BASE_URL}/index"
+    url = f"{BASE_URL}/repository/index"
 
     response = requests.post(url)
 
@@ -42,31 +54,50 @@ def list_models():
         models = response.json()
 
         print(json.dumps(models, indent=4))
-
     else:
         print(f"Failed to list models. Status code: {response.status_code}")
 
+
+def list_config_model(model):
+    url = f"{BASE_URL}/models/{model}/config"
+
+    print(url)
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        config = response.json()
+
+        print(json.dumps(config, indent=4))
+    else:
+        print(f"Failed to list model config. Status code: {response.status_code}")
 
 def main():
     parser = argparse.ArgumentParser(description='Manage models')
     subparsers = parser.add_subparsers(dest='command', required=True)
 
     load_parser = subparsers.add_parser('load', help='Load models')
-    load_parser.add_argument('models', nargs='+', help='Names of models to load')
+    load_parser.add_argument('model', type=str, help='Name of model to load')
+    load_parser.add_argument('--config', type=str, required=False, help='Config of model to load')
 
     unload_parser = subparsers.add_parser('unload', help='Unload models')
-    unload_parser.add_argument('models', nargs='+', help='Names of models to unload')
+    unload_parser.add_argument('model', type=str, help='Name of model to unload')
 
     subparsers.add_parser('list', help='List all models')
+
+    config_parser = subparsers.add_parser('config', help='List config')
+    config_parser.add_argument('model', type=str, help='Name of model to list config')
 
     args = parser.parse_args()
 
     if args.command == 'load':
-        load_models(args.models)
+        load_model(args.model, args.config)
     elif args.command == 'unload':
-        unload_models(args.models)
+        unload_model(args.model)
     elif args.command == 'list':
         list_models()
+    elif args.command == 'config':
+        list_config_model(args.model)
 
 
 if __name__ == "__main__":
