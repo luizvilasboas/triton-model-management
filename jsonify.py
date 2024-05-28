f1966dc02bca16baa0a9a2705e2f6ed729dc7be6
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-05-28 09:30:21 -0300

    Update jsonify.py to use Google's protobuf

--- jsonify.py
+++ jsonify.py
@@ -3,6 +3,7 @@ from google.protobuf.json_format import MessageToJson
 import config_pb2
 import json
 
+
 def pbtxt_to_dict(pbtxt_file):
     message = config_pb2.ModelConfig()
 
@@ -14,5 +15,10 @@ def pbtxt_to_dict(pbtxt_file):
         message, including_default_value_fields=True, preserving_proto_field_name=True)
 
     json_content = json.loads(json_content)
+
+    if 'backend' in json_content and not json_content['backend']:
+        del json_content['backend']
+    if 'instance_group' in json_content and not json_content['instance_group']:
+        del json_content['instance_group']
 
     return json_content
