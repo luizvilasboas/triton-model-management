86c08117be40018245ab076f0a8c29c9441e5236
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-05-29 14:03:39 -0300

    Update so model ensemble can be able to be created dynamically

--- jsonify.py
+++ jsonify.py
@@ -22,3 +22,126 @@ def pbtxt_to_dict(pbtxt_file):
         del json_content['instance_group']
 
     return json_content
+
+
+def create_ensemble_config(model_names: list[str]) -> tuple[dict, dict]:
+    ensemble_config = {
+        "name": "yolov8n_ensemble",
+        "platform": "ensemble",
+        "input": [
+            {
+                "name": "images",
+                "dims": [1, 3, 640, 640],
+                "data_type": "TYPE_FP32"
+            }
+        ],
+        "output": [
+            {
+                "name": "num_detections",
+                "data_type": "TYPE_INT32",
+                "dims": [1]
+            },
+            {
+                "name": "detection_boxes",
+                "dims": [1000, 4],
+                "data_type": "TYPE_FP32"
+            },
+            {
+                "name": "detection_scores",
+                "dims": [1000],
+                "data_type": "TYPE_FP32"
+            },
+            {
+                "name": "detection_classes",
+                "data_type": "TYPE_INT32",
+                "dims": [1000]
+            }
+        ],
+        "ensemble_scheduling": {
+            "step": []
+        }
+    }
+
+    for idx, model_name in enumerate(model_names):
+        step = {
+            "model_name": model_name,
+            "model_version": -1,
+            "input_map": {
+                "images": "images",
+            },
+            "output_map": {
+                "output0": f"{model_name}_output"
+            }
+        }
+
+        ensemble_config["ensemble_scheduling"]["step"].append(step)
+
+    postprocess_step = {
+        "model_name": "postprocess",
+        "model_version": -1,
+        "input_map": {},
+        "output_map": {
+            "detection_classes": "detection_classes",
+            "num_detections": "num_detections",
+            "detection_boxes": "detection_boxes",
+            "detection_scores": "detection_scores"
+        }
+    }
+
+    for idx, model_name in enumerate(model_names):
+        postprocess_step["input_map"][f"INPUT_{idx}"] = f"{model_name}_output"
+
+    ensemble_config["ensemble_scheduling"]["step"].append(postprocess_step)
+
+    postprocess_inputs = []
+    for idx in range(len(model_names)):
+        input_entry = {
+            "name": f"INPUT_{idx}",
+            "dims": [-1, -1, -1],
+            "data_type": "TYPE_FP32"
+        }
+
+        postprocess_inputs.append(input_entry)
+
+    postprocess_config = {
+        "name": "postprocess",
+        "backend": "python",
+        "input": postprocess_inputs,
+        "output": [
+            {
+                "name": "num_detections",
+                "data_type": "TYPE_INT32",
+                "dims": [1]
+            },
+            {
+                "name": "detection_boxes",
+                "dims": [1000, 4],
+                "data_type": "TYPE_FP32"
+            },
+            {
+                "name": "detection_scores",
+                "dims": [1000],
+                "data_type": "TYPE_FP32"
+            },
+            {
+                "name": "detection_classes",
+                "data_type": "TYPE_INT32",
+                "dims": [1000]
+            }
+        ],
+        "instance_group": [
+            {
+                "count": 0,
+                "kind": "KIND_CPU"
+            }
+        ]
+    }
+
+    return ensemble_config, postprocess_config
+
+
+if __name__ == '__main__':
+    model_names = ["yolov8n_mug", "yolov8n"]
+    ensemble_json, postprocess_json = create_ensemble_config(model_names)
+    # print(json.dumps(ensemble_json, indent=4))
+    print(json.dumps(postprocess_json, indent=4))
--- manage.py
+++ manage.py
@@ -1,15 +1,16 @@
 import argparse
 import requests
 import json
+from jsonify import pbtxt_to_dict, create_ensemble_config
+
 
 BASE_URL = "http://localhost:8000/v2"
 
 
-def load_model(model, config):
+def load_model(model, config = None):
     if config:
-        from jsonify import pbtxt_to_dict
-
         config = pbtxt_to_dict(config)
+
         config = json.dumps(config)
 
         payload = {
@@ -62,8 +63,6 @@ def list_models():
 def list_config_model(model):
     url = f"{BASE_URL}/models/{model}/config"
 
-    print(url)
-
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -74,13 +73,45 @@ def list_config_model(model):
         print(f"Failed to list model config. Status code: {response.status_code}")
 
 
+def load_model_ensemble(models):
+    ensemble_json, postprocess_json = create_ensemble_config(models)
+
+    configs = {
+        "postprocess": postprocess_json,
+        "yolov8n_ensemble": ensemble_json,
+    }
+
+    for model, config in configs.items():
+        config = json.dumps(config)
+
+        payload = {
+            "parameters": {
+                "config": config
+            }
+        }
+
+        url = f"{BASE_URL}/repository/models/{model}/load"
+
+        print(f"Loading model: {model}")
+
+        if config:
+            response = requests.post(url, json=payload)
+        else:
+            response = requests.post(url)
+
+        if response.status_code == 200:
+            print(f"Successfully loaded model: {model}")
+        else:
+            print(f"Failed to load model: {model}. Status code: {response.status_code}")
+
+
 def main():
     parser = argparse.ArgumentParser(description='Manage models')
     subparsers = parser.add_subparsers(dest='command', required=True)
 
     load_parser = subparsers.add_parser('load', help='Load models')
     load_parser.add_argument('model', type=str, help='Name of model to load')
-    load_parser.add_argument('config', type=str, help='Config of model to load')
+    load_parser.add_argument('--config', type=str, required=False, help='Config of model to load')
 
     unload_parser = subparsers.add_parser('unload', help='Unload models')
     unload_parser.add_argument('model', type=str, help='Name of model to unload')
@@ -89,6 +120,9 @@ def main():
 
     config_parser = subparsers.add_parser('config', help='List config')
     config_parser.add_argument('model', type=str, help='Name of model to list config')
+
+    ensemble_parser = subparsers.add_parser('ensemble', help='Load model ensemble')
+    ensemble_parser.add_argument('models', nargs='+', help='Model to load into ensemble')
 
     args = parser.parse_args()
 
@@ -100,6 +134,8 @@ def main():
         list_models()
     elif args.command == 'config':
         list_config_model(args.model)
+    elif args.command == 'ensemble':
+        load_model_ensemble(args.models)
 
 
 if __name__ == "__main__":
